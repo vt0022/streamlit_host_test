355e77c138ab93124417d1bda800b42262ca822d
TranTienDuc <trantienduc@gmail.com>
2023-04-14 16:26:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import av
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 
 RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:23.21.150.121"]}]}
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
+
 class VideoProcessor:
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
